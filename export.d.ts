472204c1548dce7a9d8b85cb1c315fac20789bff
ronze <ron.zeidman@gmail.com>
2015-12-25 11:41:53 +0200

    testing different typings

---
+++ export.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by Ron on 25/12/2015.
+ */
+
+export {Auth, SATELLIZER_PROVIDERS} from './src/auth';
+export {Config} from './src/config';
--- src/export.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Ron on 25/12/2015.
- */
-
-export {Auth, SATELLIZER_PROVIDERS} from './auth';
-export {Config} from './config';
