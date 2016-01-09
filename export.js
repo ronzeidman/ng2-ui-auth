9c48c6986a3085104f53fe1ea4ac57afce29931c
ronze <ron.zeidman@gmail.com>
2016-01-09 18:52:23 +0200

    rename

--- export.js
+++ export.js
@@ -1,4 +1,4 @@
-System.register(['./src/auth', './src/config', './src/jwtHttp'], function(exports_1) {
+System.register(['./src/auth', './src/config'], function(exports_1) {
     return {
         setters:[
             function (auth_1_1) {
@@ -10,11 +10,6 @@ System.register(['./src/auth', './src/config', './src/jwtHttp'], function(export
             function (config_1_1) {
                 exports_1({
                     "Config": config_1_1["Config"]
-                });
-            },
-            function (jwtHttp_1_1) {
-                exports_1({
-                    "JwtHttp": jwtHttp_1_1["JwtHttp"]
                 });
             }],
         execute: function() {
