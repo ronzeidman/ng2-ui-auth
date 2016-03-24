304ef5bd30f9147d0675261c9e8f7f31435a7613
ronze <ron.zeidman@gmail.com>
2016-03-24 16:26:23 +0200

    refs

--- dist/ng2-ui-auth.d.ts
+++ dist/ng2-ui-auth.d.ts
@@ -1,4 +1,4 @@
-export { Auth, NG2_UI_AUTH_PROVIDERS } from './auth';
-export { Config } from './config';
-export { Shared } from './shared';
-export { JwtHttp } from './jwtHttp';
+export { Auth, NG2_UI_AUTH_PROVIDERS } from '../src/auth';
+export { Config } from '../src/config';
+export { Shared } from '../src/shared';
+export { JwtHttp } from '../src/jwtHttp';
