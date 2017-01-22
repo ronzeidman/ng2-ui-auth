0a0b9f14ec8c5f998085ff0856192ed4f3048a3d
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2017-01-22 14:02:06 +0000

    [popup service] add missing `throw` operator.

--- src/popup.service.ts
+++ src/popup.service.ts
@@ -4,11 +4,11 @@ import {assign} from './utils';
 import {ConfigService, IPopupOptions} from './config.service';
 import 'rxjs/add/observable/interval';
 import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/observable/throw';
 import 'rxjs/add/observable/empty';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/take';
 import 'rxjs/add/operator/takeWhile';
-
 
 /**
  * Created by Ron on 17/12/2015.
