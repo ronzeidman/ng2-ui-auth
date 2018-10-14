f3b9bb6e80660b896704892e575933627f714973
ronze <ron.zeidman@gmail.com>
2018-10-14 21:37:48 +0300

    added oauthOptions to the oauth 1 flow

--- projects/ng2-ui-auth/src/lib/oauth1.service.ts
+++ projects/ng2-ui-auth/src/lib/oauth1.service.ts
@@ -1,12 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
-import { buildQueryString, joinUrl } from './utils';
 import { IOauth1Options } from './config-interfaces';
 import { ConfigService } from './config.service';
-import { PopupService } from './popup.service';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { IOauthService } from './oauth-service';
+import { PopupService } from './popup.service';
+import { buildQueryString, joinUrl } from './utils';
 
 @Injectable()
 export class Oauth1Service implements IOauthService {
@@ -15,24 +15,22 @@ export class Oauth1Service implements IOauthService {
   open<T extends object | string = any>(oauthOptions: IOauth1Options, userData: object): Observable<T> {
     const serverUrl = this.config.options.baseUrl ? joinUrl(this.config.options.baseUrl, oauthOptions.url) : oauthOptions.url;
 
-    return this.http
-      .post<object>(serverUrl, oauthOptions)
-      .pipe(
-        switchMap(
-          authorizationData =>
-            this.popup.open(
-              [oauthOptions.authorizationEndpoint, buildQueryString(authorizationData)].join('?'),
-              oauthOptions,
-              this.config.options.cordova
-            ),
-          (authorizationData, oauthData) => ({ authorizationData, oauthData })
-        ),
-        switchMap(({ authorizationData, oauthData }) => this.exchangeForToken<T>(oauthOptions, authorizationData, oauthData, userData))
-      );
+    return this.http.post<object>(serverUrl, oauthOptions).pipe(
+      switchMap(
+        authorizationData =>
+          this.popup.open(
+            [oauthOptions.authorizationEndpoint, buildQueryString(authorizationData)].join('?'),
+            oauthOptions,
+            this.config.options.cordova
+          ),
+        (authorizationData, oauthData) => ({ authorizationData, oauthData })
+      ),
+      switchMap(({ authorizationData, oauthData }) => this.exchangeForToken<T>(oauthOptions, authorizationData, oauthData, userData))
+    );
   }
 
   private exchangeForToken<T>(oauthOptions: IOauth1Options, authorizationData: object, oauthData: object, userData: object) {
-    const body = { authorizationData, oauthData, userData };
+    const body = { oauthOptions, authorizationData, oauthData, userData };
     const { withCredentials, baseUrl } = this.config.options;
     const { method = 'POST', url } = oauthOptions;
     const exchangeForTokenUrl = baseUrl ? joinUrl(baseUrl, url) : url;
